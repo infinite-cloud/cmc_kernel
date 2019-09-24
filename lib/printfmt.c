9d280e7180a9ee79d49909337a1f0b3e5b179198
infinite-cloud <54478207+infinite-cloud@users.noreply.github.com>
2019-09-24 20:20:30 +0300

    Complete lab2

--- lib/printfmt.c
+++ lib/printfmt.c
@@ -197,7 +197,7 @@ vprintfmt(void (*putch)(int, void*), void *putdat, const char *fmt, va_list ap)
 			base = 10;
 			goto number;
 
-		// unsigned decimal
+		// (unsigned) decimal
 		case 'u':
 			num = getuint(&ap, lflag);
 			base = 10;
@@ -206,10 +206,9 @@ vprintfmt(void (*putch)(int, void*), void *putdat, const char *fmt, va_list ap)
 		// (unsigned) octal
 		case 'o':
 			// Replace this with your code.
-			putch('X', putdat);
-			putch('X', putdat);
-			putch('X', putdat);
-			break;
+			num = getuint(&ap, lflag);
+			base = 8;
+			goto number;
 
 		// pointer
 		case 'p':
