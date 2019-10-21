167d81b6d884b3a3574b8b39d37ae91fb6676639
infinite-cloud <54478207+infinite-cloud@users.noreply.github.com>
2019-10-22 01:40:40 +0300

    Disable tests on JOS init

--- kern/init.c
+++ kern/init.c
@@ -45,12 +45,12 @@ i386_init(void)
 
 #ifdef CONFIG_KSPACE
 	// Touch all you want.
-	ENV_CREATE_KERNEL_TYPE(prog_test1);
-	ENV_CREATE_KERNEL_TYPE(prog_test2);
-	ENV_CREATE_KERNEL_TYPE(prog_test3);
-	ENV_CREATE_KERNEL_TYPE(prog_test4);
-	ENV_CREATE_KERNEL_TYPE(prog_test5);
-	ENV_CREATE_KERNEL_TYPE(prog_test6);
+	//ENV_CREATE_KERNEL_TYPE(prog_test1);
+	//ENV_CREATE_KERNEL_TYPE(prog_test2);
+	//ENV_CREATE_KERNEL_TYPE(prog_test3);
+	//ENV_CREATE_KERNEL_TYPE(prog_test4);
+	//ENV_CREATE_KERNEL_TYPE(prog_test5);
+	//ENV_CREATE_KERNEL_TYPE(prog_test6);
 #endif
 
 	// Schedule and run the first user environment!
