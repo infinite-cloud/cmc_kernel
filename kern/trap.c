d4d57eff816da208f859defcb52b4b896eb57865
infinite-cloud <54478207+infinite-cloud@users.noreply.github.com>
2019-11-28 13:06:42 +0300

    Update VSYS time before sending EOI to PIC

--- kern/trap.c
+++ kern/trap.c
@@ -244,8 +244,8 @@ trap_dispatch(struct Trapframe *tf)
 	if (tf->tf_trapno == IRQ_OFFSET + IRQ_CLOCK)
 	{
 		rtc_check_status();
-		pic_send_eoi(IRQ_CLOCK);
 		vsys[VSYS_gettime] = gettime();
+		pic_send_eoi(IRQ_CLOCK);
 		sched_yield();
 		return;
 	}
