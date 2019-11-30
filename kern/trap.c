8870422ce33106b940f04d014d6219e4e53c9dcc
infinite-cloud <54478207+infinite-cloud@users.noreply.github.com>
2019-11-30 13:15:44 +0300

    Remove redundant scheduler calls

--- kern/trap.c
+++ kern/trap.c
@@ -235,7 +235,6 @@ trap_dispatch(struct Trapframe *tf)
 		cprintf("Spurious interrupt on irq 7\n");
 		print_trapframe(tf);
 		pic_send_eoi(IRQ_SPURIOUS);
-		sched_yield();
 		return;
 	}
 
@@ -253,7 +252,6 @@ trap_dispatch(struct Trapframe *tf)
 	{
 		kbd_intr();
 		pic_send_eoi(IRQ_KBD);
-		sched_yield();
 		return;
 	}
 
@@ -261,7 +259,6 @@ trap_dispatch(struct Trapframe *tf)
 	{
 		serial_intr();
 		pic_send_eoi(IRQ_SERIAL);
-		sched_yield();
 		return;
 	}
 
