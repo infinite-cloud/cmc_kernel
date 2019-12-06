2960c9d5fe37ce5dd6f6d36ef29e0538f8730e1c
infinite-cloud <54478207+infinite-cloud@users.noreply.github.com>
2019-12-06 18:20:52 +0300

    Document PBKDF2

--- inc/pbkdf2.h
+++ inc/pbkdf2.h
@@ -1,3 +1,5 @@
+/* Original copyright (c) 2000-2001 by Apple Computer, Inc. */
+
 #include <inc/types.h>
 
 #ifndef PBKDF2_H
@@ -7,10 +9,37 @@
 #define UINT32_MAX ((1ULL << (8 * sizeof(uint32_t))) - 1)
 #endif
 
+/*
+ * This function should generate a pseudo random octet stream of hash_len
+ * (which is specified as an arument to pbkdf2() and should be constant
+ * for any given prf function) bytes long which is output in the buffer
+ * pointed to by rnd_ptr (the caller of this function is responsible for
+ * allocation of the buffer).
+ * The inputs are the first key_len octets pointed to by key_ptr and
+ * the first text_len octets pointed to by text_ptr; both lens can have
+ * any nonzero value.
+ */
 typedef void (*PRF)(const uint8_t *key_ptr, size_t key_len,
 	const uint8_t *text_ptr, size_t text_len,
 	uint8_t *rnd_ptr);
 
+/*
+ * This function implements PBKDF2 key derivation algorithm.
+ * The output is a derived key of derived_len bytes which is written to
+ * the buffer pointed to by derived_ptr.
+ * The caller should ensure derived_ptr is at least derived_key bytes long.
+ * The key is derived from pwd_ptr (which is pwd_len bytes long) and from
+ * salt_ptr (which is salt_len bytes long). it_count iterations are performed.
+ * The argument prf is a pointer to a pseudo RNG declared above.
+ * It should write exactly hash_len bytes into its output buffer each time it
+ * is called. The argument tmp_buffer should point to a buffer
+ * MAX(hash_len, salt_len + 4) + 2 * hash_len bytes long. The buffer is used
+ * during the calculation for intermediate results.
+ * The argument salt_ptr should be a pointer to a buffer of at least 8 random
+ * bytes (64 bits). Thus salt_len should be >= 8.
+ * For each new session a new salt should be generated.
+ * The value of it_cound should be at least 1000.
+ */
 void pbkdf2(PRF prf, size_t hash_len,
 	const void *pwd_ptr, size_t pwd_len,
 	const void *salt_ptr, size_t salt_len,
--- lib/pbkdf2.c
+++ lib/pbkdf2.c
@@ -1,6 +1,14 @@
+/* Original copyright (c) 2000-2001 by Apple Computer, Inc. */
+
 #include <inc/string.h>
 #include <inc/pbkdf2.h>
 
+/*
+ * Writes hash_len bytes into data_ptr according to PKCS #5 2.0 spec.
+ * See https://tools.ietf.org/html/rfc2898 for details.
+ * tmp_buffer is a pointer to at least MAX(hash_len, salt_len + 4) + hash_len
+ * bytes.
+ */
 static void compute_pbkdf2(PRF prf, size_t hash_len,
 	const void *pwd_ptr, size_t pwd_len,
 	const void *salt_ptr, size_t salt_len,
@@ -15,6 +23,7 @@ static void compute_pbkdf2(PRF prf, size_t hash_len,
 	out_block = (uint8_t *) tmp_buffer;
 	in_block = out_block + hash_len;
 
+	/* Set up in_block to contain salt || INT (block_num). */
 	memcpy(in_block, salt_ptr, salt_len);
 
 	in_block[salt_len + 0] = (uint8_t) (block_num >> 24);
@@ -22,18 +31,25 @@ static void compute_pbkdf2(PRF prf, size_t hash_len,
 	in_block[salt_len + 2] = (uint8_t) (block_num >> 8);
 	in_block[salt_len + 3] = (uint8_t) (block_num);
 
+	/* Calculate U1 (result goes to out_block) and copy it to
+	   result_block. */
 	result_block = (uint8_t *) data_ptr;
 	prf(pwd_ptr, pwd_len, in_block, salt_len + 4, out_block);
 	memcpy(result_block, out_block, hash_len);
 
+	/* Calculate U2 though Uit_count. */
 	for (i = 2; i <= it_count; i++)
 	{
+		/* Swap in_block and out_block pointers. */
 		tmp_block = in_block;
 		in_block = out_block;
 		out_block = tmp_block;
 
+		/* Now in_block contains Ui - 1. Calculate Ui into out_block. */
 		prf(pwd_ptr, pwd_len, in_block, hash_len, out_block);
 
+		/* Xor data in data_ptr (U1 xor U2 xor ... xor Ui - 1) with
+		   out_block (Ui). */
 		for (byte = 0; byte < hash_len; byte++)
 		{
 			result_block[byte] ^= out_block[byte];
@@ -57,8 +73,13 @@ void pbkdf2(PRF prf, size_t hash_len,
 	data_ptr = (uint8_t *) derived_ptr;
 	blk_buffer = (uint8_t *) tmp_buffer;
 
+	/* This check makes sure that the following loop ends, in case
+	   where derived_len is 64 bits, and very large. This will cause
+	   the derived_key to be the maximum size supported by pbkdf2 rather
+	   than the actual requested size. */
 	complete_blocks &= UINT32_MAX;
 
+	/* First calculate all the complete hash_len blocks required. */
 	for (block_num = 1; block_num <= complete_blocks; block_num++)
 	{
 		compute_pbkdf2(prf, hash_len, pwd_ptr, pwd_len, salt_ptr,
@@ -67,6 +88,9 @@ void pbkdf2(PRF prf, size_t hash_len,
 		data_ptr += hash_len;
 	}
 
+	/* Finally if the requested output size was not an even multiple of
+	   hash_len, calculate the final block and copy the first partial_size
+	   bytes of it to the output. */
 	if (partial_size > 0)
 	{
 		compute_pbkdf2(prf, hash_len, pwd_ptr, pwd_len, salt_ptr,
